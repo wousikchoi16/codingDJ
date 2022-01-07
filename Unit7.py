f08f72f346d3b377724aff0d2c425476e17c1086
wousikchoi16 <wousik.choi16@gmail.com>
2022-01-07 13:23:41 +0900

    add Unit7

---
+++ Unit7.py
@@ -0,0 +1,30 @@
+# Print: 숫자는 그냥 씀
+
+# 1) 제어문자: \이후 n(다음줄이동) t(tab) \(\문자자체출력)
+# 예제: \n \t \\
+print(1,2,3, sep='\n') # sep='\n' sep='\\' sep='\t' sep=',' sep=', '
+print('1\n2')
+print('Hello', '\n', 'Python', sep='') # print(Hello\nPython)
+
+# 2) end: print문 끝에는 end='/n' default임. 
+print(1, end='.')
+print(2, end='\n')
+
+# 3) 숫자 
+print(3.1, 'Python', 100)
+print(16, 9, sep=':')
+
+# 4) 큰 숫자를 콤마로 표현 
+print(100_000_000)
+
+# 5) is
+x = 1000
+y = 1000
+print(x is y) # True
+
+# 6) 행렬곱 @
+import numpy as np
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+b = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a@b)
+
