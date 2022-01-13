79b2fcadac5ac2c57524ac4acd0823715b4c7b39
wousikchoi16 <wousik.choi16@gmail.com>
2022-01-14 00:03:55 +0900

    add Unit14

---
+++ Unit14.py
@@ -0,0 +1,8 @@
+# Unit 14: IF/Else
+# 1) conditional expression: y = x if x == 10 else 0 
+# 2) 조건식: if 0<x<20: print('10')
+x = 10
+if 0 < x < 20: 
+    print('True')
+# 기타) 참/거짓: 
+#   - 거짓: 0, None, ""(빈문자열)
