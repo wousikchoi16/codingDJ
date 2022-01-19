b88e17c7153814b47940bd250eb574d3a9e3db53
wousikchoi16 <wousik.choi16@gmail.com>
2022-01-19 19:29:09 +0900

    add Unit21

---
+++ Unit21.py
@@ -0,0 +1,21 @@
+# Unit21
+# # 거북이 이동 
+import turtle as t
+# t.shape('turtle')
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.mainloop()
+
+n = 6
+t.shape('turtle')
+t.color('red')
+t.begin_fill()
+for i in range(n):
+    t.forward(100)
+    t.right
+t.end_fill()
